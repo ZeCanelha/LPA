c232c54c54f3a6e3b97a4f957624fbd18118cd03
Joel Pires <joelrogao@hotmail.com>
2017-02-15 22:42:15 +0000

    added a simple struct of a triominoe piece

--- Problema A/A.c
+++ Problema A/A.c
@@ -4,16 +4,18 @@
 int puzzle_parts[20][3];
 int free_edges[40][2];
 
-int main(int argc, char const *argv[])
-{
+struct Piece {
+	int seq[3];
+};
+
+
+int main(int argc, char const *argv[]) {
 	int num_parts = 0;
 
-	while(scanf("%d %d %d",&puzzle_parts[num_parts][0],&puzzle_parts[num_parts][1],&puzzle_parts[num_parts][2]) != EOF)
-	{
+	while(scanf("%d %d %d",&puzzle_parts[num_parts][0],&puzzle_parts[num_parts][1],&puzzle_parts[num_parts][2]) != EOF) {
 		num_parts++;
-		printf("Teste de merda\n", );
-	}
 
+	}
 
 	return 0;
 }
