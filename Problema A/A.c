905e15cf38dfa2933c12e3369bc81d357cf73861
Rui Dias <ruypedrodias94@gmail.com>
2017-02-15 16:50:28 +0000

    Só para ver se funcionava

--- Problema A/A.c
+++ Problema A/A.c
@@ -11,8 +11,9 @@ int main(int argc, char const *argv[])
 	while(scanf("%d %d %d",&puzzle_parts[num_parts][0],&puzzle_parts[num_parts][1],&puzzle_parts[num_parts][2]) != EOF)
 	{
 		num_parts++;
+		printf("Teste de merda\n", );
 	}
 
-	
+
 	return 0;
 }
