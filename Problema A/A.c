87989bc03a39ddb970b05c429699cc0f4096ad86
Joel Pires <joelrogao@hotmail.com>
2017-02-16 18:12:58 +0000

    minor fixes

--- Problema A/A.c
+++ Problema A/A.c
@@ -1,23 +1,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int puzzle_parts[20][3];
-int free_edges[40][2];
+#define MAX_PIECES 19
+
+int num_parts = 0;
 
 struct Piece {
 	int seq[3];
 };
 
-struct Playing_field_coords {int x, y;};
+struct Playing_field_coords {
+	int x, y;
+};
 
+struct Piece puzzle_parts[MAX_PIECES];
 
 int main(int argc, char const *argv[]) {
-	int num_parts = 0;
 
-	while(scanf("%d %d %d",&puzzle_parts[num_parts][0],&puzzle_parts[num_parts][1],&puzzle_parts[num_parts][2]) != EOF) {
-		num_parts++;
-
+	while(scanf("%d %d %d",&puzzle_parts[num_parts].seq[0], &puzzle_parts[num_parts].seq[1],&puzzle_parts[num_parts].seq[2]) != EOF && num_parts <= MAX_PIECES) {
 	}
 
 	return 0;
+}
+
+// rotate_piece (){
+
+}
+
+// print_game () {
+
+}
+
+// recursive_method () {
+
+}
+
+//print_available_pieces() {
+
 }
