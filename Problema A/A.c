b0e58d17a123311b025361fb95d7f9ad97ce3c8d
Joel Pires <joelrogao@hotmail.com>
2017-02-17 22:12:14 +0000

    print status of the game

--- Problema A/A.c
+++ Problema A/A.c
@@ -1,11 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /*CONSTANTS & STRUCTS ------------------------------------------------------------*/
 
-#define MAX_PIECES 19
+#define MAX_PIECES 3
 
-int num_parts = 0;
+int num_parts = 1;
 
 typedef struct {
 	int seq[3];
@@ -16,26 +17,24 @@ struct Playing_field_coords {
 };
 
 Piece available_pieces[MAX_PIECES];
-Piece playing_field[MAX_PIECES][MAX_PIECES];
+Piece playing_field[MAX_PIECES][MAX_PIECES]; /* se x e y tiverem a mesma paridade, entao a face é voltada para cima; caso contrário a face é voltada para baixo*/
 
 /* PROTOTYPES ---------------------------------------------------------------------*/
 void rotate_piece (Piece * my_piece);
+int isEmpty (int x, int y);
+void printField ();
 
 /*FUNCTIONS ---------------------------------------------------------------------*/
 int main(int argc, char const *argv[]) {
 
-	while(scanf("%d %d %d",&available_pieces[num_parts].seq[0], &available_pieces[num_parts].seq[1],&available_pieces[num_parts].seq[2]) != EOF) {
+	/*in the begining everything is empty -1*/
+	memset (playing_field, -1, sizeof(playing_field));
 
-		printf("VOU RODAR UMA VEZ!\n" );
-		rotate_piece (&available_pieces[num_parts]);
-
-		printf("%d, %d, %d\n", available_pieces[num_parts].seq[0], available_pieces[num_parts].seq[1], available_pieces[num_parts].seq[2]);
-		printf("VOU RODAR SEGUNDA VEZ!\n" );
-		rotate_piece (&available_pieces[num_parts]);
-		printf("%d, %d, %d\n", available_pieces[num_parts].seq[0], available_pieces[num_parts].seq[1], available_pieces[num_parts].seq[2]);
+	while(scanf("%d %d %d",&available_pieces[num_parts].seq[0], &available_pieces[num_parts].seq[1],&available_pieces[num_parts].seq[2]) != EOF && num_parts < MAX_PIECES) {
 		num_parts++;
 
 	}
+	printField();
 
 	return 0;
 }
@@ -51,14 +50,84 @@ void rotate_piece (Piece * my_piece){
 	return;
 }
 
-/* print_game () {
+/* before place a tile in empty position, check whether the numbers in already places tiles agree
+int isSideMatched () {
 
 }*/
+
+/*checks if position is empty*/
+int isEmpty (int x, int y) {
+	if(playing_field[x][y].seq[0] == 6)
+		return 1;
+	else
+		return 0;
+}
+
+/*put piece on the field
+int putOnField () {
+
+}*/
+
+/* print status of the game */
+void printField () {
+	int i, j, m, k,z;
+	for (i = 0; i < MAX_PIECES; i++) {
+		for (m = 0; m < 2; m++) {
+			for (j = 0; j < MAX_PIECES; j++) {
+					for (k = 0; k < 3; k++) {
+						if (i % 2 == j% 2) {
+							if (m == 0) {
+								if (k == 0){
+									printf(" ");
+								} else if (k == 1) {
+									printf("%d", playing_field[i][j].seq[1]);
+								} else {
+									printf(" ");
+								}
+							} else {
+								if (k == 0){
+									printf("%d", playing_field[i][j].seq[0]);
+								} else if (k == 1) {
+									printf(" ");
+								} else {
+									printf("%d", playing_field[i][j].seq[2]);
+								}
+							}
+						} else {
+							if (m == 0) {
+								if (k == 0){
+									printf("%d", playing_field[i][j].seq[0]);
+								} else if (k == 1) {
+									printf(" ");
+								} else {
+									printf("%d", playing_field[i][j].seq[2]);
+								}
+							} else {
+								if (k == 0){
+									printf(" ");
+								} else if (k == 1) {
+									printf("%d", playing_field[i][j].seq[1]);
+								} else {
+									printf(" ");
+								}
+							}
+						}
+					}
+				printf("|");
+			}
+			printf("\n");
+		}
+		for ( z = 0; z < num_parts; z++) {
+			printf("-----");
+		}
+		printf("\n");
+	}
+}
 
 /* recursive_method () {
+	base case:
+		colocar a peça na posição x e y no centro
 
-}*/
 
-/* print_available_pieces() {
-
+	varia consoante x e y??
 }*/
