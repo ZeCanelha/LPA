1dbc3ffb971e931492c3749a27c4b73afd25477a
Joel Pires <joelrogao@hotmail.com>
2017-03-11 23:01:59 +0000

    not solved yet

--- Problemas de Treino/problema_c.c
+++ Problemas de Treino/problema_c.c
@@ -12,44 +12,54 @@ int dynamic_matrix[5][5];
 int countPaths(int x, int y){
 
     int C1 = 0;
-    int C2 = 0;
+    int C2 = 0, k;
 
     if (dynamic_matrix[x][y] != -1) {
+        printf("CONA BOA\n" );
         return dynamic_matrix[x][y];
     }
 
-    if (x == width+1 || y == heigth+1) {
+    if (x == 0 || y == heigth+1) {
         return 1;
     }
 
-    for ( i = 0; i < n_lobos; i++) {
+    if (n_lobos == 0) {
+        C1 = countPaths(x+1, y);
+        C2 = countPaths(x, y-1);
+    } else {
+        for ( i = 0; i < n_lobos; i++) {
 
-        if (cord_lobos[i][0] != x+1) {
-            C1 = countPaths(x+1, y);
-            break;
+            if (cord_lobos[i][0] != x+1) {
+                C1 = countPaths(x+1, y);
+                break;
+            }
+
+            if (cord_lobos[i][1] != y-1) {
+                C2 = countPaths(x, y-1);
+                break;
+            }
+
         }
-
-        if (cord_lobos[i][1] != y-1) {
-            C2 = countPaths(x, y-1);
-            break;
-        }
-
     }
+    printf("CONA\n");
 
     dynamic_matrix[x][y] = C1 + C2;
+
+    for (k = 0; k < width+1; k++) {
+        printf("%d %d\n", dynamic_matrix[k][0],  dynamic_matrix[k][1]);
+    }
+    printf("\n");
 
     return dynamic_matrix[x][y];
 }
 
 int main(int argc, char const *argv[]) {
 
-    int i, k;
+    int i;
 
-    while(1)
-    {
+    while(1) {
         scanf("%d %d",&width,&heigth);
-        if ( width == 0 && heigth == 0)
-        {
+        if ( width == 0 && heigth == 0) {
             break;
         }
 
@@ -63,22 +73,15 @@ int main(int argc, char const *argv[]) {
 
         memset(dynamic_matrix, -1, sizeof(dynamic_matrix));
 
-        n_paths = countPaths(width+1, 0);
+        n_paths = countPaths(width, 0);
 
-        for (k = 0; k < width+1; k++) {
-            printf("%d %d\n", dynamic_matrix[k][0],  dynamic_matrix[k][1]);
-        }
-        printf("\n");
 
-        if ( n_paths > 1 )
-        {
+
+        if ( n_paths > 1 ) {
             printf("There are %d paths from Little Red Riding Hood's house to her grandmother's house.\n",n_paths);
-        }else if ( n_paths == 1 )
-        {
-            printf("There is one paths from Little Red Riding Hood's house to her grandmother's house.\n");
-        }
-        else
-        {
+        } else if ( n_paths == 1 ) {
+            printf("There is one path from Little Red Riding Hood's house to her grandmother's house.\n");
+        } else {
             printf("There is no path.\n");
         }
 
