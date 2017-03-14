634fca127f79cfe17dcac8915501d0bb3e836b86
Jose miguel Mendes Canelha <josemc@student.dei.uc.pt>
2017-03-14 05:13:33 +0000

    Inicio do problema Q

--- Problemas de Treino/problema_p.c
+++ Problemas de Treino/problema_p.c
@@ -5,42 +5,47 @@
 
 int n_exames = 0;
 int n_cases = 0;
-int min_colors = 0;
 int colors[26];
 int adjacency[26][26];
 int gcp(int v);
-
+int used_colors = 1;
 
 int main(int argc, char const *argv[])
 {
     int i, x, y;
-        scanf("%d %d",&n_exames, &n_cases);
+    scanf("%d %d",&n_exames, &n_cases);
 
-        memset(adjacency, 0,sizeof(adjacency));
-        memset(colors, -1,sizeof(colors));
+    memset(adjacency, 0,sizeof(adjacency));
+    memset(colors, -1,sizeof(colors));
 
-        for (i = 0; i < n_cases; i++) {
-            scanf("%d %d",&x, &y);
-            adjacency[x][y] = 1;
-            adjacency[y][x] = 1;
-        }
+    for (i = 0; i < n_cases; i++) {
+        scanf("%d %d",&x, &y);
+        adjacency[x][y] = 1;
+        adjacency[y][x] = 1;
+    }
+    if (n_exames == 0 || n_cases == 0) {
+        used_colors = 0;
+    } else {
+        gcp(0);
+    }
 
-    gcp(0);
-
-    printf("%d\n", cor);
+    printf("%d\n", used_colors);
 
     return 0;
 }
 
 int gcp(int v) {
-    int  k, feasible,cor;
+    int  cor, k, feasible;
 
     if (v == n_exames + 1) {
         return 1;
     }
 
     /*CICLO DAS CORES*/
-    for (cor = 1; cor < n_exames + 1; ++cor) {
+    for (cor = 1; cor < n_exames +1; cor++) {
+        if (cor > used_colors) {
+            used_colors = cor;
+        }
         feasible = 1;
         /*CICLO DOS NOS ADJACENTES*/
         for (k = 0; k < n_exames; k++) {
--- Problemas de Treino/problema_q.c
+++ Problemas de Treino/problema_q.c
@@ -3,23 +3,22 @@
 #include <string.h>
 #include <math.h>
 
-int n_exames = 0;
-int n_cases = 0;
-int min_colors = 0;
+int n_members = 0;
+int n_connections = 0;
 int colors[26];
+int cor = 1;
 int adjacency[26][26];
 int gcp(int v);
-
 
 int main(int argc, char const *argv[])
 {
     int i, x, y;
-        scanf("%d %d",&n_exames, &n_cases);
+        scanf("%d %d",&n_members, &n_connections);
 
         memset(adjacency, 0,sizeof(adjacency));
         memset(colors, -1,sizeof(colors));
 
-        for (i = 0; i < n_cases; i++) {
+        for (i = 0; i < n_connections; i++) {
             scanf("%d %d",&x, &y);
             adjacency[x][y] = 1;
             adjacency[y][x] = 1;
@@ -27,38 +26,35 @@ int main(int argc, char const *argv[])
 
     gcp(0);
 
-    printf("%d\n", cor);
+    printf("%d\n", n_pintados);
 
     return 0;
 }
 
 int gcp(int v) {
-    int  k, feasible,cor;
+    int  k, feasible,i;
 
-    if (v == n_exames + 1) {
+    if (v == n_members + 1) {
         return 1;
     }
 
-    /*CICLO DAS CORES*/
-    for (cor = 1; cor < n_exames + 1; ++cor) {
-        feasible = 1;
-        /*CICLO DOS NOS ADJACENTES*/
-        for (k = 0; k < n_exames; k++) {
-            if (adjacency[k][v] == 1) {
-                if (colors[k] == cor) {
-                    feasible = 0;
-                    break;
-                }
+    feasible = 1;
+    /*CICLO DOS NOS ADJACENTES*/
+    for (k = 0; k < n_members; k++) {
+        if (adjacency[k][v] == 1) {
+            if (colors[k] == cor) {
+                feasible = 0;
+                break;
             }
-        }
-
-        if (feasible == 1) {
-            colors[v] = cor;
-            if (gcp(v+1) == 1) {
-                return 1;
-            }
-            colors[v] = -1;
         }
     }
+    if (feasible == 1) {
+        colors[v] = cor;
+        if (gcp(v+1) == 1) {
+            return 1;
+        }
+        colors[v] = -1;
+    }
+
     return 0;
 }
