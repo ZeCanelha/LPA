91ae02c9a1392a7978bd1d0e3b34f19fd56882b7
Joel Pires <joelrogao@hotmail.com>
2017-03-20 23:18:04 +0000

    close to the solution

--- Problemas de LPA/problema2.c
+++ Problemas de LPA/problema2.c
@@ -3,26 +3,64 @@
 #include <string.h>
 
 int n_operations = 0;
-double max_prob = 0;
-double budget = 0;
-double dp_matrix[401][401];
-double machines[401][2];
+long int budget = 0;
+double probabilities[401];
+long int costs[401];
+int cheapest_machine = 10000000;
+long int initialBill = 0;
 
-
+long int myPow(long int a, long int b);
+double max(double a, double b) { return (a > b)? a : b; }
 int main(int argc, char const *argv[])
 {
-    int i;
+    long int i, j, z;
     scanf("%d",&n_operations);
 
-    memset(machines, -1,sizeof(machines));
-    memset(dp_matrix, 0,sizeof(dp_matrix));
-
-    for (i = 0; i < n_operations; i++) {
-        scanf("%lf %lf",&machines[i][0],&machines[i][1]);
+    for (i = 1; i < n_operations +1; i++) {
+        scanf("%lf %ld",&probabilities[i],&costs[i]);
+        initialBill += costs[i];
     }
 
-    scanf("%lf",&budget);
+    scanf("%ld",&budget);
 
-    printf("%lf\n",max_prob);
+    budget = budget - initialBill;
+
+    double dp_matrix[n_operations+1][budget+1];
+
+    /*initializing first column and row*/
+    for (i = 0; i < n_operations + 1; i++) {
+        dp_matrix[i][0] = probabilities[i];
+    }
+
+    for (i = 0; i < budget+1; i++) {
+        dp_matrix[0][i] = 1;
+    }
+    if (n_operations != 0) {
+        for ( i = 1; i < n_operations + 1; i++) {
+            for ( j = 1; j < budget + 1; j++) {
+                for (z = 1; z < j/costs[i]; z++) {
+                        dp_matrix[i][j] = max(dp_matrix[i][j], dp_matrix[i-1][j - z*costs[i]] * myPow(1 - probabilities[i], z));
+                }
+            }
+        }
+    }
+
+    for (i = 0; i < n_operations +1; i++) {
+        for ( j = 0; j < budget + 1; j++) {
+            printf("%lf ", dp_matrix[i][j]);
+        }
+        printf("\n" );
+    }
+
+    printf("%.12lf\n",dp_matrix[i-1][j-1]);
     return 0;
+}
+
+long int myPow(long int a, long int b){
+    int total = 1, i;
+    for ( i = 0; i < b; i++) {
+        total *= a;
+    }
+
+    return total;
 }
