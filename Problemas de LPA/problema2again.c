12bac8b232797a0d786bd31df5864b486806256e
Joel Pires <joelrogao@hotmail.com>
2017-03-21 16:27:10 +0000

    not working

--- Problemas de LPA/problema2again.c
+++ Problemas de LPA/problema2again.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 int n_operations = 0;
+int impossible = 0;
 long int budget = 0;
 double probabilities[401];
 long int costs[401];
@@ -12,9 +13,10 @@ long int initialBill = 0;
 long int calculaMax(long int i, long int j, double dp_matrix[n_operations+1][budget+1]);
 long int myPow(long int a, long int b);
 double max(double a, double b) { return (a > b)? a : b; }
+
 int main(int argc, char const *argv[])
 {
-    long int i, j;
+    long int i, j, r;
     scanf("%d",&n_operations);
 
     for (i = 1; i < n_operations +1; i++) {
@@ -27,20 +29,20 @@ int main(int argc, char const *argv[])
     budget = budget - initialBill;
 
     double dp_matrix[n_operations+1][budget+1];
-
-    memset(dp_matrix, 0, sizeof(dp_matrix));
-    if (n_operations != 0) {
-        for (i = 0; i < budget+1; i++) {
-            dp_matrix[0][i] = 1;
-        }
-        for (i = 0; i < n_operations+1; i++) {
-            dp_matrix[i][0] = probabilities[i];
+    if (n_operations != 0 || impossible != 0) {
+        for (i = 1; i < n_operations+1; i++) {
+            dp_matrix[0][i] = 0;
         }
 
+        for (i = 1; i < budget+1; i++) {
+            dp_matrix[i][1] = probabilities[1];
+        }
 
-        for ( i = 1; i < n_operations + 1; i++) {
-            for ( j = 1; j < budget + 1; j++) {
-                dp_matrix[i][j] = calculaMax(i, j, dp_matrix);
+        for ( r = 1; r < budget + 1; r++) {
+            printf("%ld\n", r);
+            for ( j = 1; j < n_operations + 1; j++) {
+                printf("J: %ld\n", j);
+                dp_matrix[r][j] = calculaMax(r, j, dp_matrix);
             }
         }
     }
@@ -58,17 +60,18 @@ int main(int argc, char const *argv[])
 
 long int calculaMax(long int i, long int j, double dp_matrix[n_operations+1][budget+1]) {
     int z, maximo = 0;
-    for (z = 1; z < j/costs[i]; z++) {
-        if (maximo < dp_matrix[i-1][j - z*costs[i]] * (1-myPow(1 - probabilities[i], z))){
-            maximo = dp_matrix[i-1][j - z*costs[i]] * (1-myPow(1 - probabilities[i], z));
-        }
+    for (z = 1; z < i/costs[j]; z++) {
+            if (maximo < dp_matrix[i-costs[j]*z][j-1] * (1-myPow(1 - probabilities[j], z))){
+                maximo = dp_matrix[i-costs[j]*z][j-1] * (1-myPow(1 - probabilities[j], z));
+            }
+
     }
     return maximo;
 }
 
 long int myPow(long int a, long int b){
-    int total = 1, i;
-    for ( i = 0; i < b; i++) {
+    int total = 1, l;
+    for ( l = 0; l < b; l++) {
         total *= a;
     }
 
