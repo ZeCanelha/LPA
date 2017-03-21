bca80de93ff9d9e4a3d1726e66e3c15f6b8de771
Joel Pires <joelrogao@hotmail.com>
2017-03-21 19:26:47 +0000

    wut happened

--- Problemas de LPA/problema2.c
+++ Problemas de LPA/problema2.c
@@ -3,18 +3,14 @@
 #include <string.h>
 
 int n_operations = 0;
-int impossible = 0;
 long int budget = 0;
 double probabilities[401];
 long int costs[401];
-int cheapest_machine = 10000000;
-long int initialBill = 0;
 
-long int calculaMax(long int i, long int j, double dp_matrix[n_operations+1][budget+1]);
+
 double myPow(double a, long int b);
 long int sum( long int j);
 double product(long int j);
-double max(double a, double b) { return (a > b)? a : b; }
 
 int main(int argc, char const *argv[])
 {
@@ -24,19 +20,17 @@ int main(int argc, char const *argv[])
 
     for (i = 1; i <= n_operations; i++) {
         scanf("%lf %ld",&probabilities[i],&costs[i]);
-        /*initialBill += costs[i];*/
     }
 
     scanf("%ld",&budget);
 
-    /*budget = budget - initialBill;*/
 
     double dp_matrix[budget+1][n_operations+1];
     long int redundantCopies[n_operations+1];
 
-    if (n_operations != 0 || impossible != 0) {
+    if (n_operations != 0) {
         for (j = 1; j <= n_operations; j++) {
-            for (b = 0; b < sum(j)-1; b++) {
+            for (b = 0; b <= (sum(j)-1); b++) {
                 dp_matrix[b][j] = 0;
             }
             dp_matrix[sum(j)][j] = product(j);
@@ -47,28 +41,32 @@ int main(int argc, char const *argv[])
         }
         for (j = 2; j <= n_operations; j++) {
             dp_matrix[budget][j] = product(j);
-            for (b = sum(j-1) + 1; b <= budget - costs[j]; b++) {
-                tmp = dp_matrix[b][j-1]*myPow((1 - (1 - probabilities[j])), (budget-b)/costs[j]);
+
+            for (b = (sum(j-1) + 1); b <= (budget - costs[j]); b++) {
+                tmp = dp_matrix[b][j-1]*(1 - myPow((1 - probabilities[j]), (budget-b)/costs[j]));
                 if (tmp > dp_matrix[budget][j]) {
                     dp_matrix[budget][j] = tmp;
                     redundantCopies[j] = (budget-b)/costs[j];
                 }
             }
+
         }
-    }
-    /*
-    printf("-------------------MATRIZ DINAMICA---------------\n" );
-    for (i = 1; i <= n_operations; i++) {
-        for ( j = 1; j <= budget; j++) {
-            printf("%.12lf ", dp_matrix[i][j]);
-        }
-        printf("\n" );
     }
 
-    printf("-------------------NUMERO DE MAQUINAS PARA CADA SETOR ----------\n" );
-    for (i = 1; i < n_operations; i++) {
-        printf("%ld ", redundantCopies[i]);
+    /*printf("-------------------MATRIZ DINAMICA---------------\n" );
+    for (i = 1; i <= n_operations; i++) {
+        for ( j = 1; j <= budget; j++) {
+            printf("%.32lf ", dp_matrix[i][j]);
+        }
+        printf("\n" );
     }*/
+
+    printf("-------------------NUMERO DE MAQUINAS PARA CADA SETOR ----------\n" );
+    for (i = 1; i <= n_operations; i++) {
+        printf("%ld ", redundantCopies[i]);
+    }
+
+    printf("\n" );
     printf("%.12lf\n",dp_matrix[budget][n_operations]);
     return 0;
 }
@@ -76,7 +74,7 @@ int main(int argc, char const *argv[])
 double myPow(double a, long int b){
     double total = 1;
     int l;
-    for ( l = 0; l < b; l++) {
+    for ( l = 1; l <= b; l++) {
         total *= a;
     }
 
@@ -84,7 +82,7 @@ double myPow(double a, long int b){
 }
 
 long int sum(long int j){
-    int total = 0, i;
+    long int total = 0, i;
     for (i = 1; i <= j; i++) {
         total += costs[i];
     }
