225329bcfbf4364687ad6063af1d9547e56c7e29
Jose miguel Mendes Canelha <josemc@student.dei.uc.pt>
2017-05-19 16:07:15 +0100

    Invalid F

--- Problemas de LPA/ProblemaD.c
+++ Problemas de LPA/ProblemaD.c
@@ -4,9 +4,9 @@
 
 
 int can_fire(int ingoing_nodes[6][6], int state[6], int n_transactions, int n_places, int i);
-int build_tree(int n_transactions, int n_places, int d_matrix[6][6],int trigger_matrix[4], int curr_state[100][6],int init_state, int ingoing_nodes[6][6], int used_nodes[100][6]);
-void check_dominance( int state[6], int curr_state[100][6], int n_places , int curr_pos, int new_state[6]);
-int is_cycle( int current_state[6], int n_places , int used_nodes[100][6], int curr_pos);
+void build_tree(int n_transactions, int n_places, int d_matrix[6][6],int trigger_matrix[4], int current[6],int init_state, int ingoing_nodes[6][6], int used_nodes[1000][6]);
+void check_dominance( int state[6], int curr_state[1000][6], int n_places , int curr_pos, int new_state[6]);
+int is_cycle( int current_state[6], int n_places , int used_nodes[1000][6], int curr_pos);
 
 
 int main(int argc, char const *argv[])
@@ -22,8 +22,7 @@ int main(int argc, char const *argv[])
     scanf("%d %d",&n_places,&n_transactions);
     getchar();
 
-    int curr_state[100][6];
-    int used_nodes[100][6];
+    int used_nodes[1000][6];
 
     int ingoing_nodes[6][6];
     int outgoing_nodes[6][6];
@@ -60,22 +59,23 @@ int main(int argc, char const *argv[])
             }
         }
     }
-    printf("[");
     for( k = 1; k <= n_places; k++ )
     {
-        scanf("%d",&curr_state[0][k]);
-        printf(" %d ", curr_state[0][k]);
-        used_nodes[0][k] = curr_state[0][k];
-    }
-    printf("]\n");
-
-    for (i = 1; i <= n_transactions; i++) {
-      for (j = 1;  j <= n_places; j++) {
-        d_matrix[i][j] = outgoing_nodes[i][j] - ingoing_nodes[i][j];
-      }
+        scanf("%d",&used_nodes[0][k]);
+        printf("%d ", used_nodes[0][k]);
     }
 
-    build_tree(n_transactions,n_places,d_matrix,trigger_matrix,curr_state,0, ingoing_nodes, used_nodes);
+    printf("\n");
+
+    for (i = 1; i <= n_transactions; i++)
+    {
+        for (j = 1;  j <= n_places; j++)
+        {
+            d_matrix[i][j] = outgoing_nodes[i][j] - ingoing_nodes[i][j];
+        }
+    }
+
+    build_tree(n_transactions,n_places,d_matrix,trigger_matrix,used_nodes[0],0, ingoing_nodes, used_nodes);
 
     return 0;
 }
@@ -83,66 +83,90 @@ int main(int argc, char const *argv[])
 int can_fire( int ingoing_nodes[6][6] , int state[6], int n_transactions, int n_places, int i)
 {
     int j;
-    int trigger = 0;
     for ( j = 1; j <= n_places; j++ )
     {
-
         if( ingoing_nodes[i][j] > 0 )
         {
-            if ( state[j] >= ingoing_nodes[i][j] )
+            if ( state[j] < ingoing_nodes[i][j] )
             {
-                trigger = 1;
-            }
-            else
-            {
-                trigger = 0;
+                return 0;
             }
         }
     }
-    return trigger;
+    return 1;
 }
 
 
-int build_tree(int n_transactions, int n_places, int d_matrix[6][6], int trigger_matrix[4],int curr_state[100][6],int init_state, int ingoing_nodes[6][6], int used_nodes[100][6] )
+void build_tree(int n_transactions, int n_places, int d_matrix[6][6], int trigger_matrix[4],int current[6],int init_state, int ingoing_nodes[6][6], int used_nodes[1000][6] )
 {
     int i,j,k;
     int sum = 0;
-
+    int aux = 0;
     int new_state[6];
+    int aux_state[6];
 
     for ( i = 1; i <= n_transactions; i++ )
     {
-        if (can_fire(ingoing_nodes,curr_state[init_state],n_transactions,n_places,i) == 1)
+        if (can_fire(ingoing_nodes,current,n_transactions,n_places,i) == 1)
         {
+            aux++;
             trigger_matrix[i] = 1;
 
-            /* Calcular o State para cada trigger */
             for ( j = 1; j <= n_places; j++ )
             {
                 for ( k = 1; k <= n_transactions; k++ )
                 {
                     sum += trigger_matrix[k] * d_matrix[k][j];
                 }
-                curr_state[init_state+1][j] = sum + curr_state[init_state][j];
+                aux_state[j] = sum + current[j];
 
                 sum = 0;
             }
             trigger_matrix[i] = 0;
 
+            if ( aux < 2)
+            {
+                init_state++;
+            }
+
             /* Verficar se é um estado dominate */
-            check_dominance(curr_state[init_state+1],curr_state,n_places,init_state,new_state);
+            check_dominance(aux_state,used_nodes,n_places,init_state,new_state);
 
             /* Imprimir */
-            printf("[");
+
+            for ( j = 1;j <= n_places;j++)
+            {
+                if (used_nodes[init_state-1][j] == -1)
+                {
+                    new_state[j] = -1;
+                }
+            }
+
+            for ( j = 0; j < init_state; j++ )
+            {
+                printf(" ");
+            }
+
             for ( j = 1; j <= n_places; j++ )
             {
-
                 if ( new_state[j] == -1)
-                    printf(" w ");
+                {
+                    if ( j == n_places )
+                        printf("w");
+                    else
+                        printf("w ");
+                }
                 else
-                    printf(" %d ", new_state[j]);
+                {
+                    if ( j == n_places )
+                        printf("%d", new_state[j]);
+                    else
+                        printf("%d ", new_state[j]);
+                }
             }
-            printf("]\n");
+            printf("\n");
+
+
 
             /* Verificar se é ciclo: caso não seja ,recursão. */
 
@@ -150,18 +174,19 @@ int build_tree(int n_transactions, int n_places, int d_matrix[6][6], int trigger
             if ( is_cycle(new_state,n_places,used_nodes,init_state) == 1 )
             {
                 for ( j = 1; j  <= n_places; j++ )
-                    used_nodes[init_state+1][j] = new_state[j];
+                    used_nodes[init_state][j] = new_state[j];
 
-                build_tree(n_transactions,n_places,d_matrix,trigger_matrix,curr_state,init_state+1, ingoing_nodes,used_nodes);
+                build_tree(n_transactions,n_places,d_matrix,trigger_matrix, aux_state, init_state, ingoing_nodes,used_nodes);
 
             }
+
 
         }
     }
 
 }
 
-void check_dominance( int state[6], int curr_state[100][6], int n_places , int curr_pos, int new_state[6])
+void check_dominance( int state[6], int curr_state[1000][6], int n_places , int curr_pos, int new_state[6])
 {
     int i, j,k;
     int flag = 0;
@@ -169,7 +194,7 @@ void check_dominance( int state[6], int curr_state[100][6], int n_places , int c
     int cond = 0;
 
 
-    for ( i = 0; i <= curr_pos; i++ )
+    for ( i = 0; i < curr_pos; i++ )
     {
         for ( j = 1; j <= n_places; j++ )
         {
@@ -200,11 +225,11 @@ void check_dominance( int state[6], int curr_state[100][6], int n_places , int c
     }
 }
 
-int is_cycle( int current_state[6], int n_places , int used_nodes[100][6] , int curr_pos)
+int is_cycle( int current_state[6], int n_places , int used_nodes[1000][6] , int curr_pos)
 {
     int i,j;
     int checker = 0;
-    for ( i = 0 ; i <= curr_pos; i++ )
+    for ( i = 0 ; i < curr_pos; i++ )
     {
         for ( j = 1; j <= n_places; j++ )
         {
@@ -213,6 +238,7 @@ int is_cycle( int current_state[6], int n_places , int used_nodes[100][6] , int 
         }
         if ( checker == n_places )
             return 0;
+
         else
             checker = 0;
     }
